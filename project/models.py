a11317c0d4dc5f0910fb8cd0b751eeaae9bef17c
Tryd0g0lik <work80@mail.ru>
2024-12-07 16:39:54 +0700

    [master]: 'model_init' added, models ubdated, was

--- project/models.py
+++ project/models.py
@@ -1,1 +1,32 @@
 import sqlalchemy as sq
+from project.apps import bcrypt
+from project.models_more.model_init import Base
+
+class Users(Base):
+    """This is a model Users of table in db"""
+    __tablename__ = "users"
+    
+    id = sq.Column(sq.Integer, primary_key=True)
+    balance = sq.Column(sq.Float, nullable=False)
+    commision_rate = sq.Column(sq.Float, nullable=False)
+    webhook_url = sq.Column(sq.String(255), nullable=False)
+    wallet_address = sq.Column(sq.String(255), unique=True)
+    
+    def __str__(self):
+        return f"Index: {self.id}, Balance: {self.balance}, Commissiom:\
+{self.commision_rate}"
+
+class Transaction(Base):
+    __tablename__ = "transaction"
+    
+    id = sq.Column(sq.Integer, primary_key=True)
+    amount = sq.Column(sq.Float, nullable=False)
+    commission = sq.Column(sq.Float, nullable=False)
+    # "ожидание", "подтверждена", "отменена", "истекла"
+    status = sq.Column(sq.String(28), nullable=False)
+    
+    def __str__(self):
+        return f" Index: {self.id}, Commission: {self.commission} Status: \
+{self.status}"
+
+
---
+++ project/models_more/model_init.py
@@ -0,0 +1,13 @@
+"""
+Here is an abstract table. This table is inherit
+from the flask's 'DeclarativeBase.
+'"""
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    """
+    Basic class
+    Here is an abstract table. This table is inherit
+    from the flask's 'DeclarativeBase.
+    """
+    pass
