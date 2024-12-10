c56d3470db516b6767b4cbabadfa95aa6e1eecf6
Tryd0g0lik <work80@mail.ru>
2024-12-10 10:43:01 +0700

    [admin]: First commit of  new branch

---
+++ project/forms/user_sessions/edit_form.py
@@ -0,0 +1,9 @@
+"""
+This page is a form for edit the user data.
+"""
+from flask_wtf import FlaskForm
+from wtforms import (StringField,
+                     SubmitField,
+                     validators)
+class EditorUserData(FlaskForm):
+    pass
